a9014b386096fb99d81258cd05f4868aeefffd26
shahriartamim2 <202011039@ye.butex.edu.bd>
2024-09-02 20:24:00 +0600

    feat - added 2d array create methos in heap - two methods

---
+++ Mastering Data Structures & Algorithms using C and C++/6. Array/7. 2D Array/array2d.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int **A;
+    A = new int*[3];
+    A[0] = new int[4];
+    A[1] = new int[4];
+    A[2] = new int[4];
+
+
+    // Print the arrays
+    for (int i = 0; i < 3; ++i) {  // Loop through each row
+        for (int j = 0; j < 4; ++j) {  // Loop through each element in the row
+            cout << A[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    // Free the allocated memory
+    for (int i = 0; i < 3; ++i) {
+        delete[] A[i];
+    }
+
+    return 0;
+}
