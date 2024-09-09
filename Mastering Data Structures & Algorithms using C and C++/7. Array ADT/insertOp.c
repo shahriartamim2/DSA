cef62751ed7544c99f611abe16f6e68bbec3dae1
shahriartamim2 <202011039@ye.butex.edu.bd>
2024-09-09 20:47:46 +0600

    feat - appendOp updated

---
+++ Mastering Data Structures & Algorithms using C and C++/7. Array ADT/insertOp.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Array
+{
+    int *A;
+    int length;
+    int size;
+};
+
+void Append(struct Array *arr, int x){
+    arr->A[arr->length] = x;
+    arr->length++;
+}
+
+void Display(struct Array *arr){
+    int i;
+    for (i = 0; i < arr->length; i++)
+    {
+        printf("%d\t", arr->A[i]);
+    };
+}
+
+    int main()
+{
+    struct Array arr;
+    int i;
+    arr.A = (int *)malloc(5 * sizeof(int));
+    arr.length = 5;
+    for (i = 0; i < arr.length; i++)
+    {
+        arr.A[i] = i+1;
+    };
+    Append(&arr, 9);
+    Display(&arr);
+
+    return 0;
+};
